2e077512d12a31b66636c285bedb475348fcd20a
barondedios <realbarondedios@gmail.com>
2025-10-15 11:53:35 +0800

    Modified the css, images, and app.jsx to my own assets

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,12 @@ function App() {
             <span className="navbar-toggler-icon" />
           </button>
           <a href="index.html" className="navbar-brand mx-auto mx-lg-0">
-            First
+            Aaron
           </a>
           <div className="d-flex align-items-center d-lg-none">
             <i className="navbar-icon bi-telephone-plus me-3" />
             <a className="custom-btn btn" href="#section_5">
-              120-240-9600
+              +(63)918-906-3002
             </a>
           </div>
           <div className="collapse navbar-collapse" id="navbarNav">
@@ -61,7 +61,7 @@ function App() {
             <div className="d-lg-flex align-items-center d-none ms-auto">
               <i className="navbar-icon bi-telephone-plus me-3" />
               <a className="custom-btn btn" href="#section_5">
-                120-240-9600
+                +(63)918-906-3002
               </a>
             </div>
           </div>
@@ -78,13 +78,13 @@ function App() {
                 <div className="hero-text">
                   <div className="hero-title-wrap d-flex align-items-center mb-4">
                     <img
-                      src="images/happy-bearded-young-man.jpg"
+                      src="images/my-face-1.jpeg"
                       className="avatar-image avatar-image-large img-fluid"
                       alt=""
                     />
-                    <h1 className="hero-title ms-3 mb-0">Hello, World!</h1>
+                    <h1 className="hero-title ms-3 mb-0">I'm Aaron Bacani</h1>
                   </div>
-                  <h2 className="mb-4">I’m available for freelance work.</h2>
+                  <h2 className="mb-4">I’m a Digital Artist and an aspiring Game Developer.</h2>
                   <p className="mb-4">
                     <a className="custom-btn btn custom-link" href="#section_2">
                       Let's begin
@@ -93,9 +93,9 @@ function App() {
                 </div>
               </div>
               <div className="col-lg-5 col-12 position-relative">
-                <div className="hero-image-wrap" />
+                <div className="" />
                 <img
-                  src="images/portrait-happy-excited-man-holding-laptop-computer.png"
+                  src="images/"
                   className="hero-image img-fluid"
                   alt=""
                 />
@@ -104,7 +104,7 @@ function App() {
           </div>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path
-              fill="#535da1"
+              fill="#646464ff"
               fillOpacity={1}
               d="M0,160L24,160C48,160,96,160,144,138.7C192,117,240,75,288,64C336,53,384,75,432,106.7C480,139,528,181,576,208C624,235,672,245,720,240C768,235,816,213,864,186.7C912,160,960,128,1008,133.3C1056,139,1104,181,1152,202.7C1200,224,1248,224,1296,197.3C1344,171,1392,117,1416,90.7L1440,64L1440,0L1416,0C1392,0,1344,0,1296,0C1248,0,1200,0,1152,0C1104,0,1056,0,1008,0C960,0,912,0,864,0C816,0,768,0,720,0C672,0,624,0,576,0C528,0,480,0,432,0C384,0,336,0,288,0C240,0,192,0,144,0C96,0,48,0,24,0L0,0Z"
             />
